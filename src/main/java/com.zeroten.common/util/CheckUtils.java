4a06ef9ace8648e25ff89bc6b77075e0fcbff761
冯猛 <2119154772@qq.com>
2019-12-12 18:27:15 +0800

    编写工具类和测试用例

--- src/main/java/com.zeroten.common/util/CheckUtils.java
+++ src/main/java/com.zeroten.common/util/CheckUtils.java
@@ -1,4 +1,64 @@
 package com.zeroten.common.util;
 
 public class CheckUtils {
+
+
+    /**
+     *  String...strings 为可变参数，你可以认为它是String[] strings。如果strings中有
+     *  任意一个空字符串，则返回false，否则返回true
+     */
+
+    public static boolean isAnyEmpty(String... strings){
+        if(strings != null && strings.length > 0){
+            for(String str : strings){
+                if(str == null){
+                    continue;
+                }else if(str.length() == 0){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    /**
+     *判断引用类型数组是否为空，为空或null则返回true，否则返回false。
+     */
+    public static boolean isEmpty(Object[] arr){
+        if( arr == null || arr.length ==0){
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     *  判断str1字符串是否相等，相等返回true，否则返回false。当其中一个是null时返回false。
+     */
+    public static boolean equals(String str1,String str2) {
+//        if (str1 == null || str2 == null) {
+//            return false;
+//        } else if (str1 == str2) {
+//            return true;
+//        }
+//        return false;
+        if(str1 == str2){
+            return true;
+        }
+        return false;
+
+    }
+
+
+    /**
+     * 判断n1和n2的值是否相等，相等返回true，否则返回false。当其中一个是null时返回false。
+     */
+    public static boolean equals(Integer n1,Integer n2){
+        if(n1 == null || n2 == null){
+            return false;
+        }else if(n1.byteValue() == n2.byteValue()){
+            return true;
+        }
+        return false;
+    }
 }
--- src/main/test/CheckUtilsTest.java
+++ src/main/test/CheckUtilsTest.java
@@ -1,2 +1,39 @@
+import com.zeroten.common.util.CheckUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CheckUtilsTest {
+    @Test
+    public void testIsAnyEmpty(){
+        Assert.assertFalse(CheckUtils.isAnyEmpty(new String[]{"1", "2"," ",null,"","3"}));
+        Assert.assertTrue(CheckUtils.isAnyEmpty(new String[]{}));
+        Assert.assertTrue(CheckUtils.isAnyEmpty(null));
+    }
+
+    @Test
+    public void testIsEmpty(){
+        Assert.assertFalse(CheckUtils.isEmpty(new Integer[]{1,2,3}));
+        Assert.assertFalse(CheckUtils.isEmpty(new Integer[]{1,2,3,null}));
+        Assert.assertTrue(CheckUtils.isEmpty(new Integer[]{}));
+        Assert.assertTrue(CheckUtils.isEmpty(null));
+    }
+
+    @Test
+    public void testStringEquals(){
+        Assert.assertTrue(CheckUtils.equals("str1","str1"));
+        Assert.assertFalse(CheckUtils.equals("str1",new String("str1")));
+        Assert.assertFalse(CheckUtils.equals("str1","str2"));
+        Assert.assertFalse(CheckUtils.equals("str1",null));
+
+    }
+
+    @Test
+    public void testIntegerEquals(){
+        Assert.assertTrue(CheckUtils.equals(1,1));
+        Assert.assertFalse(CheckUtils.equals(1,2));
+        Assert.assertTrue(CheckUtils.equals(1,new Integer(1)));
+        Assert.assertFalse(CheckUtils.equals(1,null));
+
+    }
+
 }
